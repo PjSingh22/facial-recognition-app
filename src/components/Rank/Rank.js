fd66405b85307a0ab9fa163e81018b3b80a0ad81
Prabhjot Singh <pjsingh1359@outlook.com>
2019-10-19 10:21:22 -0700

    implimented back end into front end

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = _ => {
+const Rank = ({name, entries}) => {
   return (
     <div>
       <div className='white f3'>
-        {'Johnny, your current rank is...'}
+        {`${name}, your current rank is...`}
       </div>
       <div className='white f1'>
-        {'#5'}
+        {entries}
       </div>
     </div>
   );
